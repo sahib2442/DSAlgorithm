bbdcc735c6d4d2dfd440211655f4e462c31da735
sahib2442 <kadersahib1234@gmail.com>
2022-09-19 15:35:15 +0530

    [sahib] Add: bubble sort

---
+++ BubbleSort/BubbleSort.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.ComponentModel;
+
+namespace BubbleSort
+{
+    class BubbleSortt
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = { 2,3,1,4 };
+            int temp;
+            for (int i= 0; i <= arr.Length - 2; i++)
+            {
+                for (int j = 0; j <= arr.Length - 2; j++)
+                {
+                    if (arr[i] > arr[i + 1])
+                    {
+                        temp = arr[i + 1];
+                        arr[i + 1] = arr[i];
+                        arr[i] = temp;
+                    }
+                }
+            }
+            Console.WriteLine("Sorted:");
+            foreach (int p in arr)
+                Console.Write(p + " ");
+            Console.Read();
+        }
+    }
+}
---
+++ BubbleSort/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.ComponentModel;
+
+namespace BubbleSort
+{
+    class BubbleSort
+    {
+        static void Master(string[] args)
+        {
+            int[] arr = { 2, 3, 1, 4 };
+            int temp;
+            for (int i = 0; i <= arr.Length - 2; i++)
+            {
+                Console.WriteLine("Sorted:");
+                foreach (int p in arr)
+                Console.Write(p + " ");
+                Console.Read();
+            }
+        }
+    }
+}
+
