8a0074b6f79d6a3cca4ee4035b67430daba1834d
sahib2442 <kadersahib1234@gmail.com>
2022-09-19 14:40:46 +0530

    [sahib] Add: binary search

---
+++ BinarySearch/BinarySearch.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.IO;
+namespace LinkedListProblem1.DsAndAlgorithm
+{
+    public class BinarySearch
+    {
+        public void Search(string filePath)
+        {
+            string data = File.ReadAllText(filePath);
+            string[] words = data.Split(",");
+            Console.WriteLine("Enter word to search");
+            string search = Console.ReadLine();
+            foreach (var item in words)
+            {
+                if (search.ToLower().Equals(item.ToLower()))
+                {
+                    Console.WriteLine("word found");
+                    return;
+                }
+            }
+            Console.WriteLine("word not found");
+        }
+    }
+}
---
+++ BinarySearch/Program.cs
@@ -0,0 +1,15 @@
+﻿using LinkedListProblem1.DsAndAlgorithm;
+
+const string WORDS_FILE_PATH = @"D:\RFP\DsAndAlgorithm\Words.txt";
+while (true)
+{
+    Console.WriteLine("\n Select Program\n 1.Search");
+    int option = Convert.ToInt32(Console.ReadLine());
+    switch (option)
+    {
+        case 1:
+            BinarySearch binarySearch = new BinarySearch();
+            binarySearch.Search(WORDS_FILE_PATH);
+            break;
+    }
+}
