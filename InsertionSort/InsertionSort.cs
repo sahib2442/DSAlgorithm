0bd72b8bc160a53a3fa7e0a9ec5c5d47a93eb017
sahib2442 <kadersahib1234@gmail.com>
2022-09-19 15:15:59 +0530

    [sahib] Add: insertion sort

---
+++ InsertionSort/InsertionSort.cs
@@ -0,0 +1,36 @@
+﻿using System;
+namespace InsertionSort
+{
+    public class InsertionSort
+    {
+        public static void Main(string[]args)
+        { 
+         int[] arr = new int[4] {2,1,3,4} ;
+         int n = 4, i, j, val, flag;
+         Console.WriteLine("Insertion Sort");
+         Console.Write("Initial array is: ");
+         for (i = 0; i < n; i++) {
+            Console.Write(arr[i] + " ");
+         }
+         for (i = 1; i < n; i++)
+            {
+            val = arr[i];
+            flag = 0;
+            for (j = i - 1; j >= 0 && flag != 1; )
+                {
+               if (val < arr[j]) {
+                  arr[j + 1] = arr[j];
+                  j--;
+                  arr[j + 1] = val;
+               }
+               else flag = 1;
+            }
+         }
+         Console.Write("\nSorted Array is: ");
+         for (i = 0; i < n; i++) {
+            Console.Write(arr[i] + " ");
+         }
+      }
+   }
+}
+
---
+++ InsertionSort/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+namespace InsertionSort
+{
+    class program
+    {
+        public static void Master(string[] args)
+        {
+            int[] arr = new int[4] {2,3,1,4};
+            int n = 4, i;
+            Console.WriteLine("Insertion Sort");
+            Console.Write("Initial array is: ");
+            for (i = 0; i < n; i++)
+            {
+                Console.Write(arr[i] + " ");
+            }
+        }
+    }
+}
+
+
+
+
